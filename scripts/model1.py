fd678078e2bd0aed4bb97a91b9c583cf5ffe18dc
calanzer <calanzer@gmail.com>
2019-08-12 23:01:02 -0700

    Repushing changes for getting player rating

--- scripts/model1.py
+++ scripts/model1.py
@@ -6,17 +6,31 @@ from datetime import date
 
 data = pandas.read_csv("../data/spi_matches.csv")
 
+#Takes two parameters, name and the path of the file you want to query. Returns a dataframe with the players stats.
+def getPlayerRating(name, filepath):
+    fifadata = pandas.read_csv(filepath)
+    result = fifadata[fifadata['NAME'].str.contains(name)]
+    if len(result) > 0:
+        result = result.nsmallest(1, 'RATING')
+    #print(result)
+    print(result['RATING'].values.tolist())
+
+
+
+
+
 #Adds new column with the number of days since the game
 def addDaysSinceGame(data):
     today = date.today()
     date_format = "%m/%d/%Y"
     data['date'] = pandas.to_datetime(data['date'])
-    data['daysSi/nce'] = today - data['date']
+    data['daysSince'] = today - data['date']
 
 #Main function
 def main():
-    addDaysSinceGame(data)
-    print(data)
+    getPlayerRating('Antonio Valencia', '../data/FIFA11.csv')
+    #addDaysSinceGame(data)
+
 
 
 #Run Main
